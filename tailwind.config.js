ab050f79d738315353b5bca5d787fbd5109c631e
ShahinTheDeveloper <inboxtoshahin@gmail.com>
2023-06-20 13:05:09 +0600

    Upload Assignment

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    content: ["./**/*.{html,js}"],
+    theme: {
+        container: {
+            padding: {
+                DEFAULT: "1rem",
+                sm: "2rem",
+                lg: "4rem",
+                xl: "5rem",
+                "2xl": "6rem",
+            },
+        },
+        extend: {
+            fontFamily: {
+                Archivo: ["Archivo", "Arial", "sans-serif"],
+            },
+        },
+    },
+    plugins: [],
+};
